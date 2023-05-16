fa0ce6e540cce77369e0b9c530d3b13d68a97334
chuvaaa <acsmia@gmail.com>
2023-05-17 07:24:57 +0900

    feat : table 추가 및 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,22 @@ CREATE TABLE PRODUCT
     PRIMARY KEY (id)
 );
 
+CREATE TABLE MEMBER
+(
+    email VARCHAR(30)   NOT NULL,
+    password     VARCHAR(30)   NOT NULL,
+    created_dt   DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (email)
+);
+
 INSERT INTO PRODUCT (name, image, price)
 VALUES ( '히로', '/image/hero.JPG', '1000000' );
 
 INSERT INTO PRODUCT (name, image, price)
 VALUES ( '봉구', '/image/bonggu.JPG', '1100000' );
+
+INSERT INTO MEMBER (email, password)
+VALUES ('hero@naver.com', '1111');
+
+INSERT INTO MEMBER (email, password)
+VALUES ('bonggu@naver.com', '1111');
